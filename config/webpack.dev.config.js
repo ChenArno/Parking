1cfede90e1c4b7c0bb0252c5457bf5f759006378
ChenArno <517625126@qq.com>
2019-12-24 10:09:08 +0800

    增加端口配置

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -4,7 +4,7 @@
  * @Author: chenArno
  * @Date: 2019-12-12 14:59:42
  * @LastEditors  : chenArno
- * @LastEditTime : 2019-12-20 11:20:17
+ * @LastEditTime : 2019-12-24 10:04:53
  */
 const merge = require('webpack-merge')
 const common = require('./webpack.common.config')
@@ -13,6 +13,8 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const portfinder = require('portfinder')
+const PORT = 8000
 
 const webpackDevConfig = merge(common, {
   mode: 'development',
@@ -22,7 +24,8 @@ const webpackDevConfig = merge(common, {
   devServer: {
     contentBase: path.resolve(__dirname, '../dist'),
     open: true,
-    port: 8000,
+    host: 'localhost',
+    port: PORT,
     compress: true,
     hot: true,
     // 如果使用webpack-dev-server，需要设为true，禁止显示devServer的console信息
@@ -36,19 +39,40 @@ const webpackDevConfig = merge(common, {
       inject: 'body',
       hash: false
     }),
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin()
     // HotModuleReplacementPlugin是webpack热更新的插件，
     // 设置devServer.hot为true，并且在plugins中引入HotModuleReplacementPlugin插件即可。
     // 还需要注意的是我们开启了hot，
     // 那么导出不能使用chunkhash，需要替换为hash。
-    new FriendlyErrorsWebpackPlugin({
-      compilationSuccessInfo: {
-        messages: [`Your application is running here:http://127.0.0.1:8000`]
-      },
-      // onErrors:,
-      clearConsole: true
-    })
+    // new FriendlyErrorsWebpackPlugin({
+    //   compilationSuccessInfo: {
+    //     messages: [`Your application is running here:http://127.0.0.1:8000`]
+    //   },
+    //   // onErrors:,
+    //   clearConsole: true
+    // })
   ]
 })
 
-module.exports = webpackDevConfig
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = PORT
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err)
+    } else {
+      webpackDevConfig.devServer.port = port
+      webpackDevConfig.plugins.push(
+        new FriendlyErrorsWebpackPlugin({
+          compilationSuccessInfo: {
+            messages: [
+              `Your application is running here: http://${webpackDevConfig.devServer.host}:${port}`
+            ]
+          },
+          // onErrors: utils.createNotifierCallback(),
+          clearConsole: true
+        })
+      )
+      resolve(webpackDevConfig)
+    }
+  })
+})
