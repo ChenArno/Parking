a3436af6395696346f801dba357807042668f29a
ChenArno <517625126@qq.com>
2020-01-13 10:01:31 +0800

    修改时间类工具，增加时间判断

--- src/utils/DateFormat.ts
+++ src/utils/DateFormat.ts
@@ -3,7 +3,7 @@
  * @Author: chenArno
  * @Date: 2020-01-06 16:17:55
  * @LastEditors  : chenArno
- * @LastEditTime : 2020-01-07 14:15:58
+ * @LastEditTime : 2020-01-13 10:01:13
  */
 
 interface Option {
@@ -21,30 +21,34 @@ class DateFormat {
     date = date === undefined ? new Date() : date
     let str: Date = typeof date === 'number' ? new Date(date) : date as Date
     fmt = fmt || 'yyyy-MM-dd HH:mm:ss'
-    let options: Option = {
-      'y': str.getFullYear(),
-      'M': str.getMonth() + 1,
-      'd': str.getDate(),
-      'q': Math.floor((str.getMonth() + 3) / 3),// 季度
-      'w': str.getDay(), // 星期，0-6
-      'H': str.getHours(),
-      'h': str.getHours() % 12 === 0 ? 12 : str.getHours() % 12, // 12小时制
-      'm': str.getMinutes(),
-      's': str.getSeconds(),
-      'S': str.getMilliseconds()
+    if (date instanceof Date) { // 判断时间类型
+      let options: Option = {
+        'y': str.getFullYear(),
+        'M': str.getMonth() + 1,
+        'd': str.getDate(),
+        'q': Math.floor((str.getMonth() + 3) / 3),// 季度
+        'w': str.getDay(), // 星期，0-6
+        'H': str.getHours(),
+        'h': str.getHours() % 12 === 0 ? 12 : str.getHours() % 12, // 12小时制
+        'm': str.getMinutes(),
+        's': str.getSeconds(),
+        'S': str.getMilliseconds()
+      }
+      let week: Array<string> = ['天', '一', '二', '三', '四', '五', '六']
+      for (let i in options) {
+        fmt = fmt.replace(new RegExp(i + '+', 'g'), (m: string): string => {
+          let val: string = options[i] + ''
+          if (i === 'w') return (m.length > 2) ? '星期' : '周' + week[val as any]
+          for (let j = 0, len = val.length; j < m.length - len; j++) {
+            val = '0' + val
+          }
+          return m.length === 1 ? val : val.substring(val.length - m.length)
+        })
+      }
+      return fmt
+    } else {
+      return date
     }
-    let week: Array<string> = ['天', '一', '二', '三', '四', '五', '六']
-    for (let i in options) {
-      fmt = fmt.replace(new RegExp(i + '+', 'g'), (m: string): string => {
-        let val: string = options[i] + ''
-        if (i === 'w') return (m.length > 2) ? '星期' : '周' + week[val as any]
-        for (let j = 0, len = val.length; j < m.length - len; j++) {
-          val = '0' + val
-        }
-        return m.length === 1 ? val : val.substring(val.length - m.length)
-      })
-    }
-    return fmt
   }
 
   /**
